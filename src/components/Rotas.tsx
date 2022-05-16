3772ace4a32cd13b106c543f4d3101d65b489822
gjantsch <jantsch@gmail.com>
2022-05-16 16:16:55 -0300

    Add rotas as state

--- src/components/Rotas.tsx
+++ src/components/Rotas.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {Rota} from './Rota'
 
-type RotasProps = {entregaAt?:any}
-type RotasState = { rotas:any, entregaAt?:any}
+type RotasProps = {rotas?:any, entregaAt?:any}
+type RotasState = {rotas:any, entregaAt?:any}
 
 export class Rotas extends React.Component<RotasProps, RotasState> {
 
   constructor(props:any) {
     super(props)
 
-    this.setState({entregaAt: this.props.entregaAt})
+    this.state = {entregaAt: this.props.entregaAt, rotas: []}
 
   }
 
