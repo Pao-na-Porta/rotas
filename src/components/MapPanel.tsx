ff53da9689e039272952f4d2c44b3dbd46c64654
gjantsch <jantsch@gmail.com>
2022-05-31 13:23:07 -0300

    Remove animações

--- src/components/MapPanel.tsx
+++ src/components/MapPanel.tsx
@@ -7,25 +7,28 @@ import {marcadoresState} from "../atoms/Marcadores"
 import {useRecoilValue} from "recoil"
 import {MarkerCustom} from "./MarkerCustom"
 
-export const MapPanel = () =>  {
+export const MapPanel = () => {
 
   const marcadores = useRecoilValue(marcadoresState)
 
   return <MapContainer className='main-container'
-                center={[-30.032, -50.800]}
-                zoom={10}
-                maxZoom={18}
-                scrollWheelZoom={true}>
+                       center={[-30.032, -50.800]}
+                       zoom={10}
+                       maxZoom={18}
+                       zoomAnimation={false}
+                       markerZoomAnimation={false}
+                       scrollWheelZoom={true}>
     <TileLayer
       id='mapbox/streets-v11'
       tileSize={512}
       zoomOffset={-1}
+      updateWhenZooming={false}
       accessToken='pk.eyJ1IjoiZ3VzdGF2by1qYW50c2NoIiwiYSI6ImNrbWV5dDczYTB3YW8yeW14OHBncTJ1OGwifQ.Q7RrB_5akhYzcqQ62cncUg'
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url='https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ3VzdGF2by1qYW50c2NoIiwiYSI6ImNrbWV5dDczYTB3YW8yeW14OHBncTJ1OGwifQ.Q7RrB_5akhYzcqQ62cncUg'
     />
-    {marcadores.map((id:any) => {
-        return <MarkerCustom marcadorId={id} key={'MarkerCustom-' + id}/>
+    {marcadores.map((id: any) => {
+      return <MarkerCustom marcadorId={id} key={'MarkerCustom-' + id}/>
     })}
   </MapContainer>
 
